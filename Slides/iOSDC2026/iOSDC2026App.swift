a1077c26eae268d875e49603736358c25a666347
yutailang0119 <yutailang0119@gmail.com>
2026-08-23 21:04:49 +0900

    Use semantic styles for the presentation content view

--- Slides/iOSDC2026/iOSDC2026App.swift
+++ Slides/iOSDC2026/iOSDC2026App.swift
@@ -17,8 +17,8 @@ struct iOSDC2026App: App {
   /// Edit the view here if you'd like to set environments, overlay views or background views.
   var presentationContentView: some View {
     SlideRouterView(slideIndexController: Self.configuration.slideIndexController)
-      .background(.white)
-      .foregroundColor(.black)
+      .background(.background)
+      .foregroundStyle(.primary)
   }
 
   var body: some Scene {

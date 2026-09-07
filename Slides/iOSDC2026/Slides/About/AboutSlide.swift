767572ea7982308816d114275bc7a95028c47dab
yutailang0119 <yutailang0119@gmail.com>
2026-09-08 00:20:00 +0900

    Restructure the viznut item in AboutSlide

--- Slides/iOSDC2026/Slides/About/AboutSlide.swift
+++ Slides/iOSDC2026/Slides/About/AboutSlide.swift
@@ -19,13 +19,14 @@ struct AboutSlide: View {
       Item("8,000Hz と 8bitが前提") {
         Item("シンセも音源ファイルもサンプリングも不要")
       }
-      Item("2011年 viznut “Algorithmic symphonies from one line of code -- how and why?”") {
+      Item("“Algorithmic symphonies from one line of code -- how and why?”") {
+        Item("2011年 viznut")
         Item {
           Link(
             destination: URL(string: "https://countercomplex.blogspot.com/2011/10/algorithmic-symphonies-from-one-line-of.html")!) {
               Text("https://countercomplex.blogspot.com/2011/10/algorithmic-symphonies-from-one-line-of.html")
+                .multilineTextAlignment(.leading)
             }
-            .font(.system(size: 32, weight: .medium, design: .default))
             .underline()
         }
       }
@@ -40,7 +41,7 @@ struct AboutSlide: View {
     その結果が、そのまま波形の1サンプルになります
     ここでの前提は8,000Hz と 8bitであることだけ
     シンセも音源ファイルもサンプリングもありません
-    2011年にviznutが公開したものが始まりです
+    2011年にviznut、ヴィズナットが公開したものが始まりです
     """
   }
 }

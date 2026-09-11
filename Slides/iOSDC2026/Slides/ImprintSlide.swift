5e8102c87ebed64dd411ae1d5081f5a667f63764
yutailang0119 <yutailang0119@gmail.com>
2026-09-11 10:22:38 +0900

    Restructure ImprintSlide around a one-shot gong

--- Slides/iOSDC2026/Slides/ImprintSlide.swift
+++ Slides/iOSDC2026/Slides/ImprintSlide.swift
@@ -23,31 +23,29 @@ struct ImprintSlide: View {
   )
 
   var body: some View {
-    CenteringLayout {
-      VStack(spacing: 8) {
-        Text("Enjoy Bytebeat")
-        Text("Enjoy iOSDC")
-        Text("Thanks!")
-      }
-      .font(.system(size: 160, weight: .bold, design: .default))
-    }
-    .background {
+    ZStack {
       switch phase {
       case .initial:
         EmptyView()
       case .dora, .silent:
+        WaveformView(
+          controller: controller,
+          style: .trace
+        )
         TitleLayout(
           text: phase.expression,
           size: 40
         )
-        .foregroundStyle(.tertiary)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background {
-          WaveformView(
-            controller: controller,
-            style: .trace
-          )
+        .foregroundStyle(phase.expressionStyle)
+      }
+      CenteringLayout {
+        VStack(spacing: 8) {
+          Text("Enjoy Bytebeat")
+          Text("Enjoy iOSDC")
+          Text("Thanks!")
         }
+        .font(.system(size: 160, weight: .bold, design: .default))
+        .foregroundStyle(phase.messageStyle)
       }
     }
     .background(.background)
@@ -94,6 +92,20 @@ struct ImprintSlide: View {
 private extension ImprintSlide.SlidePhasedState {
   var expression: String {
     "(128+(((((((((((((((((t<24064)?(24064-t):0))>>6)*((((t<24064)?(24064-t):0))>>6))>>8)*((t<64)?t:64))>>6)*(((((((t*466)>>6)^(-((((t*466)>>6)>>7)&1)))&127)-64)*34)+(((((((t*474)+2069)>>6)^(-(((((t*474)+2069)>>6)>>7)&1)))&127)-64)*13)))+(((((((((t<26112)?(26112-t):0))>>6)*((((t<26112)?(26112-t):0))>>6))>>8)*((t<64)?t:64))>>6)*((((((((t*166)+6151)>>6)^(-(((((t*166)+6151)>>6)>>7)&1)))&127)-64)*32)+(((((((t*602)+14371)>>6)^(-(((((t*602)+14371)>>6)>>7)&1)))&127)-64)*9))))+(((((((((t<27136)?(27136-t):0))>>6)*((((t<27136)?(27136-t):0))>>6))>>8)*((t<64)?t:64))>>6)*(((((((t*610)+12302)>>6)^(-(((((t*610)+12302)>>6)>>7)&1)))&127)-64)*21)))+(((((((((((t<27136)?(27136-t):0))>>6)*((((t<27136)?(27136-t):0))>>6))>>8)*((t<64)?t:64))>>6)*(((t>>2)<256)?(t>>2):256))>>8)*(((((((t*2849)+8220)>>6)^(-(((((t*2849)+8220)>>6)>>7)&1)))&127)-64)*9)))+(((((((((t<26112)?(26112-t):0))>>6)*((((t<26112)?(26112-t):0))>>6))>>8)*(((t>>2)<256)?(t>>2):256))>>8)*((((((((((t*166)>>6)^((t*4792)>>6))&255)-128)>>1)*16)+(((((((t*466)>>6)^((t*2849)>>6))&255)-128)>>1)*16))+(((((((((t>>1)*2654435761)^(((t>>1)*2654435761)>>15))&65535)*40503)>>9)&127)-64)*12))+(((((((((t>>1)*2654435761)^(((t>>1)*2654435761)>>15))&65535)*40503)>>9)&127)-64)*14))))+(((((((((t<24064)?(24064-t):0))>>6)*((((t<24064)?(24064-t):0))>>6))>>8)*(((t>>2)<256)?(t>>2):256))>>8)*(((((((t*456)>>6)^((t*3494)>>6))&255)-128)>>1)*16)))+(((((t<320)?(320-t):0))>>2)*(((((((((t>>6)*2654435761)^(((t>>6)*2654435761)>>15))&65535)*40503)>>9)&127)-64)*700)))+(((((t<1920)?(1920-t):0))>>4)*((((((((t*166)>>6)^(-((((t*166)>>6)>>7)&1)))&127)-64)*180)+((((((t*466)>>6)^(-((((t*466)>>6)>>7)&1)))&127)-64)*180))+((((((t*474)>>6)^(-((((t*474)>>6)>>7)&1)))&127)-64)*180))))>>16))"
+  }
+
+  var messageStyle: HierarchicalShapeStyle {
+    switch self {
+    case .initial, .silent: .primary
+    case .dora: .tertiary
+    }
+  }
+
+  var expressionStyle: HierarchicalShapeStyle {
+    switch self {
+    case .initial, .silent: .tertiary
+    case .dora: .primary
+    }
   }
 }
 
